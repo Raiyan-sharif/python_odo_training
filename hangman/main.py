7b4ff5e6b03258b30b0ed78c7fe310dc087c598e
Raiyan-sharif <raiyan.sharif.1234@gmail.com>
2023-12-11 17:13:22 +0600

    hangman

---
+++ hangman/main.py
@@ -0,0 +1,10 @@
+import random
+from hungman_words import *
+
+if __name__ == '__main__':
+    result =  random.choice(word_list)
+    # print(result)
+    s = '_'*len(result)
+    
+    
+    print(s)
